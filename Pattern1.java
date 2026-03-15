00ef0273b96a923b0072f660be2e662ad0304def
Anwesha Barad <anweshabarad8@gmail.com>
2026-03-15 23:48:37 +0530

    Added Pattern1 program

---
+++ Pattern1.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+ class Pattern1{
+    public static void main(String s[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter number of star:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+           for(int j=1;j<=i;j++){
+             System.out.print(" * ");
+           }
+            System.out.println();
+        }
+    }
+ }
+
+
+
+ /*
+ ...................
+ *
+ *  *
+ *  *  *
+ *  *  *  *
+ *  *  *  *  *
+ ''''''''''''''''''
+  */
