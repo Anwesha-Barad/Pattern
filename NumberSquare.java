7b4055d07f726816a68d3a606691e930ac996a13
Anwesha Barad <anweshabarad8@gmail.com>
2026-05-23 23:57:10 +0530

    Added NumberSquare program

---
+++ NumberSquare.java
@@ -0,0 +1,17 @@
+//Number Square
+import java.util.Scanner;
+public class NumberSquare{
+    public static void main(String s[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter number to print pattern:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(i);
+            }
+             System.out.println();
+        }
+       
+
+    }
+}
