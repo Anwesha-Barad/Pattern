992afcd974a10db0f7fe101aabb7687a1a117cda
Anwesha Barad <anweshabarad8@gmail.com>
2026-05-25 20:24:45 +0530

    Added FullStarPyramid program

---
+++ FullStarPyramid.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+ class FullStarPyramid
+ {
+    public static void main(String s[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter number of star:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            //space
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+             System.out.println();
+        }
+        sc.close();
+ 
+    }
+}
+
+/*
+--------------
+    *
+   ***
+  *****
+ *******
+*********
+
+--------------
+*/
