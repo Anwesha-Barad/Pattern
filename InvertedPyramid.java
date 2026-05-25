a3b841e6db47e35c8303ad5206455e8b2fa9e49e
Anwesha Barad <anweshabarad8@gmail.com>
2026-05-25 20:26:35 +0530

    Added InvertedPyramid program

---
+++ InvertedPyramid.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+ class  InvertedPyramid
+ {
+    public static void main(String s[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter number of star:");
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--){
+            //space
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+             System.out.println();
+        }
+        sc.close();
+ 
+    }
+}
+
+/*
+--------------
+*********
+ *******
+  *****
+   ***
+    *
+--------------
+*/
